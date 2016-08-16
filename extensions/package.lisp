390ebea627929540a94b4cc11d28d1a541933cdb
Marco Heisig <marco.heisig@fau.de>
2016-08-16 12:57:04 +0200

    new example: send-anything

--- extensions/package.lisp
+++ extensions/package.lisp
@@ -3,5 +3,8 @@
   (:use :cl :cl-mpi :static-vectors :alexandria)
   (:export
    #:mpi-send-anything
-   #:mpi-receive-anything
+   #:mpi-recv-anything
+   #:mpi-waitall-anything
+   #:mpi-isend-anything
+   #:mpi-irecv-anything
    #:mpi-broadcast-anything))
